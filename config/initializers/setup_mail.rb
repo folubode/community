2cd6db9a318f25c775dd916ae8f8b64f5d4fe8fa
Femi Olubode-Awosola <olucube@yahoo.co.uk>
2018-01-29 23:09:52 +1300

    bootstrap...

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,7 +4,7 @@
     :enable_starttls_auto => true,  #this is the important stuff!
     :address        => ENV['address'],
     :port           => 587,
-    :domain         => localhost:3000,
+    :domain         => 'localhost:3000',
     :authentication => :plain,
     :user_name      => ENV['user_name'],
     :password       => ENV['password']
