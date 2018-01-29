4e7123287651fe65b8c50fd63c60ad8d6db43504
Femi Olubode-Awosola <olucube@yahoo.co.uk>
2018-01-29 21:18:03 +1300

    bootstrap...

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
- # STATIC PAGES
+ 	# STATIC PAGES
   get '/about' => 'pages#about'
 
 
+  # DYNAMIC PAGES
+  resources :contacts
 
 
 
+
+	# APPLICATION ROOT
 	root 'pages#home'
 
 end
