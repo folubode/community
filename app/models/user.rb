1f5d5b3c0463f8fe5226d2fe41cfca0d71bd901f
Femi Olubode-Awosola <olucube@yahoo.co.uk>
2018-02-01 19:46:39 +1300

    quantity...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,21 @@
 class User < ApplicationRecord
 
+attr_accessor :stripe_card_token
 belongs_to :plan
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+ def save_with_payment
+    if valid?
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      self.stripe_customer_token = customer.id
+      save!
+    end
+  end
+
+
+
 end
--- db/migrate/20180130112736_create_plans.rb
+++ db/migrate/20180130112736_create_plans.rb
@@ -2,7 +2,7 @@ class CreatePlans < ActiveRecord::Migration[5.1]
   def change
     create_table :plans do |t|
     	t.string :name
-      t.decimal :price, :decimal, :precision => 8, :scale => 2
+      t.decimal :price, :precision => 8, :scale => 2
 
        t.timestamps
     end
